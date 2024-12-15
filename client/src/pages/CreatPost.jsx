b56316a132cf6f844ba7a0fb97dc071417f93a2e
deepakreddy78 <deepakreddy78@gmail.com>
2024-12-15 23:22:00 +0530

    first commit

---
+++ client/src/pages/CreatPost.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const CreatPost = () => {
+  return <div>CreatPost 1</div>;
+};
+
+export default CreatPost;
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import CreatPost from "./CreatPost";
+
+export { Home, CreatPost };
